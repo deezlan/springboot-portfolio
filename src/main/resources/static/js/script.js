52212e35e2241345c12d7ae82cfab9132c59cd73
Danial Eizlan Daud <64958824+deezlan@users.noreply.github.com>
2025-10-05 02:52:15 +0100

    bug: Fixed script for undisplaying loader-wrapper

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -3,7 +3,7 @@ window.addEventListener('load', () => {
     setTimeout(() => {
         document.querySelector('.loader-wrapper').style.opacity = '0';
         setTimeout(() => {
-            document.querySelector('.loader-wrapper').style.opacity = 'none';
+            document.querySelector('.loader-wrapper').style.display = 'none';
         }, 500)
     }, 2000)
 })
